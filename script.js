23d190cfae887056bf84ed91e5c4f6ac31b1eb20
k4rth1k315ht <163455822+k4rth1k315ht@users.noreply.github.com>
2024-08-07 23:46:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-function fibonacci(num) {
-// your code here
+	function fibonacci(num) {
+    if (num === 0) return 0;
+    if (num === 1) return 1;
+
+    let a = 0;
+    let b = 1;
+    let result;
+
+    for (let i = 2; i <= num; i++) {
+        result = a + b;
+        a = b;
+        b = result;
+    }
+
+    return result;
 }
 
 module.exports = fibonacci;
